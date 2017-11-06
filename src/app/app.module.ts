6017e279a6ca500aa1a3009cf778a141e304fbd2
Naris Razafimahatratra <nrazafimahatratra@valueit.fr>
2017-11-06 15:53:48 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { HomePage } from '../pages/home/home';
+import { HttpClientModule } from '@angular/common/http';
+import { ListPage } from '../pages/list/list';
+import { MyApp } from './app.component';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
